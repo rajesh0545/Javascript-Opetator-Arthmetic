a825d7f388253e5e3244a139fb04cd3d903a7271
rajesh <krajeshgowri7032@mail.com>
2022-08-16 16:16:34 +0530

    Javascript-Arthmetic

---
+++ script.js
@@ -0,0 +1,20 @@
+var x= 5;
+var y= 7;
+//adition
+console.log('x+y=', x+y);
+//sub
+console.log('x-y=', x-y);
+//Mul
+console.log('x*y=', x*y);
+//Div
+console.log('x/y=', x/y);
+//Remainder
+console.log('x%y=', x%y);
+//inc
+console.log('++x=', ++x);
+console.log('x++=', x++);
+console.log('x=', x);
+//Dec
+console.log('--x=', --x);
+console.log('x--=', x--);
+console.log('x=', x);
